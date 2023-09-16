ad93623aec72505396ff1fdde1b287affcfad3bf
David Zimmerer <d.zimmerer@gmail.com>
2023-09-17 00:43:51 +0200

    fix

--- cbcknd/server.py
+++ cbcknd/server.py
@@ -97,24 +97,29 @@ def create_app():
 
         print(json_data)
 
+        time.sleep(2)
+
         updated_evs = get_updated_events()
         print(updated_evs)
         # trigger LLM Update
 
-        blocked_time_slots = get_events_at_days(gc_service)
-        if len(llm_responses) > 0:
-            last_response = llm_responses[-1]
-            print("Updating training plan...")
-            response = get_updated_training_plan(
-                    user_profile=user_profile,
-                    user_message="",
-                    last_response=last_response,
-                    whoop_update={},
-                    blocked_time_slots=blocked_time_slots
-            )
-            print(response.__dict__)
-            update_calendar(response)
-            llm_responses.append(response)
+        if len(updated_evs) == 0:
+            return ""
+
+        # blocked_time_slots = get_events_at_days(gc_service)
+        # if len(llm_responses) > 0:
+        #     last_response = llm_responses[-1]
+        #     print("Updating training plan...")
+        #     response = get_updated_training_plan(
+        #             user_profile=user_profile,
+        #             user_message="",
+        #             last_response=last_response,
+        #             whoop_update={},
+        #             blocked_time_slots=blocked_time_slots
+        #     )
+        #     print(response.__dict__)
+        #     update_calendar(response)
+        #     llm_responses.append(response)
 
         return "Hello, World2!"
     
