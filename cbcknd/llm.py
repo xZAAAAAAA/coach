f0e04375cbac07e95d38fa33ff730753c58c35c7
Patrick Deininger <pdeininger@palantir.com>
2023-09-16 22:02:26 +0200

    refactored time prompt

--- cbcknd/llm.py
+++ cbcknd/llm.py
@@ -3,21 +3,22 @@ import prompt_templates
 from datetime import date
 
 
-def get_initial_training_plan(user_profile):
+def get_initial_training_plan(user_profile, blocked_time_slots=[]):
     input_data = {
         "personal_data": user_profile.__dict__,
-        "current_date": date.today()
+        "time": {"current_date": date.today(), "blocked_time_slots": blocked_time_slots},
     }
     prompt = prompt_templates.build_initial_prompt(input_data)
     return ask_llm(prompt)
 
 
-def get_updated_training_plan(user_profile, user_message, last_response):
+def get_updated_training_plan(user_profile, user_message, last_response, whoop_update={}, blocked_time_slots=[]):
     input_data = {
         "personal_data": user_profile.__dict__,
-        "current_date": date.today(),
+        "time": {"current_date": date.today(), "blocked_time_slots": blocked_time_slots},
         "user_message": user_message,
-        "training_plan": last_response.get_trainings_plan()
+        "training_plan": last_response.get_trainings_plan(),
+        "whoop_update": whoop_update
     }
     prompt = prompt_templates.build_update_prompt(input_data)
     return ask_llm(prompt)
