43081670452f5d1b7c90d623d33ae9f81b4fcddb
David Zimmerer <d.zimmerer@gmail.com>
2023-09-16 12:29:09 +0200

    Local llm api

--- cbcknd/llm.py
+++ cbcknd/llm.py
@@ -12,16 +12,27 @@ def load_model(model=DEFAULT_MODEL):
     model_base = model.split("/")[-1]
 
     if not os.path.exists(MODEL_DIR):
-        os.makedirs(MODEL_DIR, exists_ok=True)
+        os.makedirs(MODEL_DIR, exist_ok=True)
 
     model_path = os.path.join(MODEL_DIR, model_base)
 
-    response = requests.get(model, allow_redirects=True)
-    open(model_path, "wb").write(response.content)
+    if not os.path.exists(model_path):
+        print("Downloading model from", model, " ...")
+        response = requests.get(model, allow_redirects=True)
+        with open(model_path, "wb") as f:
+            f.write(response.content)
+        print("Downloaded model to", model_path)
 
     return Llama(model_path=model_path)
 
 
+def prompt_model(llm, prompt, num_tokens=1024):
+    resp = llm(prompt, max_tokens=num_tokens, stop=["Q:", "\n"], echo=True)
+
+    return resp["choices"][0]["text"]
+
+
 if __name__ == "__main__":
     model = load_model()
-    # print(model.generate("Hello, my name is"))
+    test_text = prompt_model(model, "Hello, my name is")
+    print(test_text)
